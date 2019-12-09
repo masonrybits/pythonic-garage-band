51d1ba8bcc4e0ab8f6d68dff8114818dfd3ba728
Ting Luo <tin831@gmail.com>
2019-12-08 16:31:06 -0800

    added create_from_data function & added more tests

--- README.md
+++ README.md
@@ -4,16 +4,19 @@
 **Version**: 1.0.0
 
 ## Overview
-An application that creates bands, musicians(who can plays instruments" by utilizing the classes feature in Python.
+An application that creates bands, musicians(who can plays instruments) by utilizing the classes feature in Python.
 
 ## Getting Started
-The user is required to run the application by running the source codes and test suites in Python3.
+The user is required to first navigating to the folder contatining the source codes. The user can then enter the virtual environment (by typing "pipenv shell") and run the source codes (by typing "python3 band.py") and test suites (by typing "pytest") in Python3.
 
 ## Architecture
-This application is built using Python.
+The pytest framewrok is installed for testing purpose. pytest library is imported to test_band.py .
 
 ## API
-n/a
+The Band class has attributes name and members, as well as a method get_members_names that returns members' names, a play_solo method that asks each member musician to play a solo, a class method to_list which returns a list of previously created Band instances, and a static method create_from_data which takes a collection of formatted data and returns a created Band instance.
+
+The Musician class has attributes name, instrument and solo. A get_instrument method that returns a string of an instrument, and a play_solo method that returns string of an instrument solo.
 
 ## Change Log
 12-06-2019 8:00 AM - Added functions.
+12-08-2019 4:30 PM - Added create_from_data function & tested.
--- test_band.py
+++ test_band.py
@@ -2,13 +2,37 @@ from band import Band, Musician, Guitarist, Bassist, Drummer
 
 import pytest
 
-def test_instrument():
+def test_instrument_guitarist():
     g = Guitarist('Ting')
     expected = 'guitar'
     actual = g.get_instrument()
     assert actual == expected
 
-def test_solo():
+def test_instrument_bassist():
+    g = Bassist('Ting')
+    expected = 'bass'
+    actual = g.get_instrument()
+    assert actual == expected
+
+def test_instrument_drummer():
+    g = Drummer('Ting')
+    expected = 'drum'
+    actual = g.get_instrument()
+    assert actual == expected
+
+def test_solo_guitar():
+    g = Guitarist('Ting')
+    expected = 'Ting plays guitar solo. '
+    actual = g.play_solo()
+    assert actual == expected
+
+def test_solo_bass():
+    g = Guitarist('Ting')
+    expected = 'Ting plays guitar solo. '
+    actual = g.play_solo()
+    assert actual == expected
+
+def test_solo_drum():
     g = Guitarist('Ting')
     expected = 'Ting plays guitar solo. '
     actual = g.play_solo()
@@ -30,7 +54,26 @@ def test_list():
     b = Band('FunBand', [Guitarist('Ting'), Bassist('Tang')])
     expected = 'The band is named FunBand, the members are Ting Tang '
     actual = b.to_list()
-    print(actual)
+    assert actual == expected
+
+def test_create_from_data_str():
+    data = """FunBand
+Ting on Guitar
+Tang on Bass
+Teng on Drum"""
+    b = Band.create_from_data(data)
+    expected = 'The band is named FunBand, the members are Ting Tang Teng '
+    actual = b.__str__()
+    assert actual == expected
+
+def test_create_from_data_repr():
+    data = """FunBand
+Ting on Guitar
+Tang on Bass
+Teng on Drum"""
+    b = Band.create_from_data(data)
+    expected = 'FunBand - [Ting - guitar - guitar solo, Tang - bass - bass solo, Teng - drum - drum solo]'
+    actual = b.__repr__()
     assert actual == expected
 
 @pytest.fixture(autouse=True)
